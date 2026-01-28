2cfaafbf4ef25b7df154209f9e468718bab96818
Sarah <Sarah.francois@epita.fr>
2026-01-28 11:46:37 +0700

    a

--- main.cpp
+++ main.cpp
@@ -7,18 +7,20 @@
 
 #include <iostream>
 using namespace std;
+#include "sorting.h"
 
 int main(int argc, char * argv[]) {
     
  
   cout<<"argc is "<<argc<<endl;
     int i,N;
-    int *a;
+    int *a = new int[argc-1];
     for(i=1;i<argc;i++){
         cout<<"argv[" << i<<"]" <<argv[i]<<endl;
+        a[i-1] = atoi(argv[i]);
       
     }
-    /* Convert to numbers*/
+          
     N=argc-1;
     cout<<"Before sorting:";
     display(a,N);
--- sorting.h
+++ sorting.h
@@ -1,3 +1,29 @@
-void swap(int &a,int &b);
-void sort(int *a,int N);
-void display(int *a,int N);
+#include <iomanip>
+
+void swap(int &a,int &b)
+{
+    int t = a;
+    a=b;
+    b=t;
+}
+
+void sort(int *a,int N)
+{
+    for(int i=0; i<N; i++)
+    {
+        for (int j=0; j<i; j++)
+        {
+            if (a[i] < a[j])
+                swap(a[i], a[j]);
+        }
+    }
+}
+
+void display(int *a,int N)
+{
+    for (int i=0; i<N; i++)
+        cout<<setw(4)<<a[i];
+    cout<<endl;
+}
+
+
