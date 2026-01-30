28a8884f8fd6dd2ebb6ede9203ec2aabd6de4a33
Sarah <Sarah.francois@epita.fr>
2026-01-30 10:17:51 +0700

    a

--- sorting.h
+++ sorting.h
@@ -7,17 +7,7 @@ void swap(int &a,int &b)
     b=t;
 }
 
-void sort(int *a,int N)
-{
-    for(int i=0; i<N; i++)
-    {
-        for (int j=0; j<i; j++)
-        {
-            if (a[i] < a[j])
-                swap(a[i], a[j]);
-        }
-    }
-}
+
 
 void display(int *a,int N)
 {
@@ -26,4 +16,47 @@ void display(int *a,int N)
     cout<<endl;
 }
 
+int min(int *a, int n)
+{
+    int m=a[0];
+    for (int i=1; i<n; i++)
+    {
+        if (m>a[i])
+        {
+            m=a[i];
+        }
+    }
+    return m;
+}
 
+void selectSort(int *a, int n)
+{
+    for (int i=0; i<n-1; i++)
+    {
+        int m=i;
+        for (int j=i; j<n; j++)
+        {
+            if (a[m]>a[j])
+                m=j;
+        }
+        swap(a[i], a[m]);
+        for (int k=0; k<n; k++)
+            cout<<setw(4)<<a[k];
+        cout<<endl;
+    }
+}
+
+
+
+void sort(int *a,int N)   //bubble sort
+{
+    selectSort(a, N);/*
+    for(int i=0; i<N; i++)
+    {
+        for (int j=0; j<i; j++)
+        {
+            if (a[i] < a[j])
+                swap(a[i], a[j]);
+        }
+    }*/
+}
